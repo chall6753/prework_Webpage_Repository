148233f01872081b969d8019f1795d926284cc08
Charles <chall70101@gmail.com>
2021-07-10 16:58:01 -0600

    add basic sections to webpage

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 const h2 = document.createElement('h2');
-h2.textContent = "This content added by JavaScript"; 
+h2.textContent = "Fort Collins Gardening 101"; 
 
 document.querySelector('body').appendChild(h2); 
